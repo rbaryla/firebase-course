170ae7d7d4f16bef246f8074a03872e0715e615d
rbaryla <robert@em4u.pl>
2020-01-02 23:11:31 +0100

    Finished section 14: Querying a Firestore Collection using AngularFire

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { Course } from '../model/course';
 
 @Component({
   selector: "about",
@@ -8,19 +6,7 @@ import { Course } from '../model/course';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-  constructor(private db: AngularFirestore) {}
+  constructor() {}
 
-  ngOnInit() {
-    this
-      .db
-      .collection('courses')
-      .snapshotChanges()
-      .subscribe(snaps => {
-        const courses: Course[] = snaps.map(snap => <Course>{
-          id: snap.payload.doc.id,
-          ...snap.payload.doc.data() as Course
-        });
-        console.log(courses);
-      });
-  }
+  ngOnInit() {}
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,26 +1,47 @@
-import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
-import {Observable} from "rxjs";
-import {map} from "rxjs/operators";
-
-
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
-    selector: 'home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+  selector: "home",
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  private corses$: Observable<Course[]>;
+  beginersCorses$: Observable<Course[]>;
+  advancedCorses$: Observable<Course[]>;
 
+  constructor(private db: AngularFirestore) {}
 
-    constructor() {
+  ngOnInit() {
+    this.corses$ = this.db
+      .collection('courses')
+      .snapshotChanges()
+      .pipe(
+        map(snaps =>
+          snaps.map(
+            snap =>
+              <Course>{
+                id: snap.payload.doc.id,
+                ...(snap.payload.doc.data() as Course)
+              }
+          )
+        )
+      );
 
-    }
+    this.beginersCorses$ = this.corses$.pipe(
+      map(courses => courses.filter(
+        course => course.categories.includes('BEGINNER')
+      ))
+    );
 
-    ngOnInit() {
-
-
-
-    }
-
+    this.advancedCorses$ = this.corses$.pipe(
+      map(courses => courses.filter(
+        course => course.categories.includes('ADVANCED')
+      ))
+    );
+  }
 }
