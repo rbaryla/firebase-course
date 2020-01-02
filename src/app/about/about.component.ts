cc5a953c7caebef2f6125a3bce4a289c54998144
rbaryla <robert@em4u.pl>
2020-01-02 01:12:50 +0100

    End of section Introduction to the Firestore NoSQL Database

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { CONFIG as config } from './../../../__private';
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+import { Course } from '../model/course';
+
+firebase.initializeApp(config);
+
+const db = firebase.firestore();
 
 @Component({
-  selector: 'about',
+  selector: "about",
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    db.collection('courses')
+      .get()
+      .then(snaps => {
+        const courses: Course[] = snaps.docs.map(snap => {
+          return <Course>{
+            id: snap.id,
+            ...snap.data()
+          };
+        });
+        console.log(courses);
+      });
   }
-
 }
