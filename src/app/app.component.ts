375ed54e7b32477567463a601975fabfcb08f86c
rbaryla <robert@em4u.pl>
2020-01-14 00:20:17 +0100

    finished(stage-32): the AngularFire Auth User Profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  isLoggedIn$: Observable<boolean>;
+  isLoggedOut$: Observable<boolean>;
 
+  constructor(private afAuth: AngularFireAuth) {}
 
-    logout() {
+  logout() {
+    this.afAuth.auth.signOut();
+  }
 
-    }
+  ngOnInit(): void {
+    this.afAuth.authState.subscribe(user => console.log(user));
 
+    this.isLoggedIn$ = this.afAuth.authState.pipe(map(user => !!user));
+    this.isLoggedOut$ = this.isLoggedIn$.pipe(map(loggedIn => !loggedIn));
+  }
 }
